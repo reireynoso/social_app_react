30a1639649933e49ae34f23f5dbe812c36039de8
reireynoso <reireynoso@gmail.com>
2020-01-26 19:05:10 -0500

    removed files and added env file for URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
+  console.log(process.env["REACT_APP_URL"])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
