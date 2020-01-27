b19f7eb4de6a94de4488c9288df26ee32ae7a949
reireynoso <reireynoso@gmail.com>
2020-01-26 21:08:16 -0500

    npm installed react-actioncable-provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 const App = () => {
   console.log(process.env["REACT_APP_URL"])
+
+  useEffect(() => {
+    // fetch(`${process.env["REACT_APP_URL"]}/messages`)   
+  }, [])
   return (
     <div className="App">
       
