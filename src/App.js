29510e656103256a069f92264f4e049d150f8f40
reireynoso <reireynoso@gmail.com>
2020-01-26 22:11:04 -0500

    wrapped App component with actioncable provider and installed methods

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
 import React, {useEffect, useState} from 'react';
-
+import { ActionCableConsumer } from 'react-actioncable-provider';
 
 const App = () => {
-  console.log(process.env["REACT_APP_URL"])
+  // console.log(process.env["REACT_APP_URL"])
+  const [user, setUser] = useState({})
+  const [messages, setMessages] = useState([])
+  const [newMessages, setNewMessages] = useState([])
+  console.log(messages)
+  console.log(newMessages)
+  const createMessage = () => {
+    fetch(`${process.env["REACT_APP_URL"]}/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+          "sender_id": 3,
+          "receiver_id": 2,
+          "content": "testing"   
+      })
+    })
+    .then(resp => resp.json())
+    // .then(newMessage => setNewMessages([...newMessages, newMessage]))
+  }
 
+  const addMessage = (message) => {
+    setNewMessages([...newMessages, message])
+  }
   useEffect(() => {
-    // fetch(`${process.env["REACT_APP_URL"]}/messages`)   
+    fetch(`${process.env["REACT_APP_URL"]}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+          "username": "example",
+          "password": "hello" 
+      })
+    })
+    .then(resp => resp.json())
+    .then(userData => {
+      setUser(userData)
+      // console.log(userData)
+      setMessages(userData.user.data.attributes.received_messages)
+    })
+
   }, [])
   return (
     <div className="App">
-      
+      <button onClick={createMessage}>Create Message</button>
+      <ActionCableConsumer
+          channel={{ channel: 'FeedChannel' }}
+          onReceived={message => {
+            console.log(message);
+            // this.addTweet(tweet)
+            addMessage(message)
+          }}
+        />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ActionCableProvider } from 'react-actioncable-provider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ActionCableProvider url={`ws://localhost:3000/cable`}>
+        <App />
+    </ActionCableProvider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
